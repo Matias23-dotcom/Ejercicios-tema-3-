85558e0c2eaa2db19ffbe13af956ed6430c36e37
Matias23-dotcom <122494852+Matias23-dotcom@users.noreply.github.com>
2023-01-11 23:33:55 -0300

    Add files via upload

---
+++ Untitled-1.java
@@ -0,0 +1,17 @@
+package ejercicio;
+
+public class Main {
+
+   public static void main(String [] args) {
+      suma (a:10, b:15, c:30);
+   }
+
+   public static int suma(int a, int b, int c) {
+      int resultado;
+      resultado= a + b + c;
+
+      System.out.printl(resultado); 
+
+
+    }
+}
---
+++ untitled-2.java
@@ -0,0 +1,20 @@
+package ejercicio;
+
+public class Main {
+
+   public static void main(String [] args) {
+      Coche miCoche= new Coche[];
+      miCoche.AñadirPuerta[]
+   }
+
+  public static int suma (int a, int b) {
+      return a + b;
+
+}
+
+class Coche {
+    public int puertas=4;
+
+    public void AñadirPuerta () {
+        this.puertas= }
+}
